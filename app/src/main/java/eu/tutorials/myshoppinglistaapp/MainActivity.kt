75a8a75076b98bf41b7fb0807a9832704b1804e3
Manika Sharma <manika.sharma10000@gmail.com>
2025-03-28 11:33:37 +0100

    Added button to add item

---
+++ app/src/main/java/eu/tutorials/myshoppinglistaapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package eu.tutorials.myshoppinglistaapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import eu.tutorials.myshoppinglistaapp.ui.theme.MyShoppingListAappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyShoppingListAappTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   Column(
+                        modifier = Modifier.fillMaxSize(),
+                       verticalArrangement = Arrangement.Center
+                   ){
+                        Button(
+                            onClick = {},
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ){
+                            Text(text = "Add Item")
+                        }
+                   }
+                }
+            }
+        }
+    }
+}
