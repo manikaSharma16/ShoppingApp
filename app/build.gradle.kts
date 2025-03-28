0fb9f61454a1b0c45dc75cbf9b41bba9a894d0a1
Manika Sharma <manika.sharma10000@gmail.com>
2025-03-28 13:13:30 +0100

    Added lazycolumn to show the list of items, list of items are not populated yet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "eu.tutorials.myshoppinglistaapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "eu.tutorials.myshoppinglistaapp"
