7a4d83838cbe2c7afbad4ea02a44a8ecc2ecd320
Manika Sharma <manika.sharma10000@gmail.com>
2025-04-17 13:27:14 +0200

    Added vector for edit and delete an item from lazycolumn

--- app/src/main/java/eu/tutorials/myshoppinglistaapp/ShoppingList.kt
+++ app/src/main/java/eu/tutorials/myshoppinglistaapp/ShoppingList.kt
@@ -11,9 +11,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -60,8 +65,8 @@ fun ShoppingListApp(){
             onClick = {showDialog=true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ){
-            val doubleNumber : (Int) -> Int = {it*2} // : (input type) -> (return type) {function def}
-            Text(doubleNumber(5).toString())
+
+            Text("Add Item")
         }
 
         LazyColumn(
@@ -145,5 +150,17 @@ fun ShoppingListItem(
             )
     ){
         Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            IconButton(onClick = { onEditClick }) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "This is the edit button of this given row")
+            }
+
+            IconButton(onClick = { onDeleteClick }) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "This is the delete button of this given row")
+            }
+        }
     }
 }
