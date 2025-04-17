7f37b8921c1a0cec37c1af330699e7764d0c153a
Manika Sharma <manika.sharma10000@gmail.com>
2025-04-17 12:10:58 +0200

    Lambda expression explained code

--- app/src/main/java/eu/tutorials/myshoppinglistaapp/ShoppingList.kt
+++ app/src/main/java/eu/tutorials/myshoppinglistaapp/ShoppingList.kt
@@ -1,12 +1,16 @@
 package eu.tutorials.myshoppinglistaapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -55,7 +60,8 @@ fun ShoppingListApp(){
             onClick = {showDialog=true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ){
-            Text(text = "Add Item")
+            val doubleNumber : (Int) -> Int = {it*2} // : (input type) -> (return type) {function def}
+            Text(doubleNumber(5).toString())
         }
 
         LazyColumn(
@@ -64,7 +70,7 @@ fun ShoppingListApp(){
                 .padding(16.dp)
         ){
             items(sItems){
-
+                ShoppingListItem(item = it, onEditClick = {}, {} )
             }
         }
     }
@@ -72,7 +78,32 @@ fun ShoppingListApp(){
     if (showDialog){
        AlertDialog(
            onDismissRequest = { showDialog=false },
-           confirmButton = { /*TODO*/ },
+           confirmButton = { Row(
+               modifier = Modifier
+                   .fillMaxWidth()
+                   .padding(8.dp),
+               horizontalArrangement = Arrangement.SpaceBetween
+           ) {
+                Button(onClick = {
+                    if(itemName.isNotBlank()){
+                        val newItem = ShoppingItem(
+                        id = sItems.size+1,
+                            name = itemName,
+                            quantity = itemQuantity.toInt()
+                        )
+
+                        sItems = sItems + newItem
+                        showDialog=false
+                        itemName=""
+                    }
+                }) {
+                    Text(text = "Add")
+                }
+               Button(onClick = { showDialog=false }) {
+                  Text(text = "Cancel")
+               }
+           }
+           },
            title = { Text("Add shopping item") },
            text = {
                Column {
@@ -80,16 +111,39 @@ fun ShoppingListApp(){
                        value = itemName,
                        onValueChange = {itemName = it},
                        singleLine = true,
-                       modifier = Modifier.fillMaxWidth().padding(8.dp)
+                       modifier = Modifier
+                           .fillMaxWidth()
+                           .padding(8.dp)
                        )
                    OutlinedTextField(
                        value = itemQuantity,
                        onValueChange = {itemQuantity = it},
                        singleLine = true,
-                       modifier = Modifier.fillMaxWidth().padding(8.dp)
+                       modifier = Modifier
+                           .fillMaxWidth()
+                           .padding(8.dp)
                    )
                }
            }
        )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick : () -> Unit,
+    onDeleteClick : () -> Unit
+){
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color(0XFF018786)),
+                shape = RoundedCornerShape(20)
+            )
+    ){
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }
